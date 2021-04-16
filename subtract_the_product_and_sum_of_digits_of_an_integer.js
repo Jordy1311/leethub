01e9ad8e8c6d37f45387bfee677d0e96e49b72c9
Jordy1311 <jordancooper1195@gmail.com>
2021-04-16 14:31:27 +1200

    Subtract the Product and Sum of Digits of an Integer - REDUCE METH

--- subtract_the_product_and_sum_of_digits_of_an_integer.js
+++ subtract_the_product_and_sum_of_digits_of_an_integer.js
@@ -3,12 +3,19 @@
  * @return {number}
  */
  var subtractProductAndSum = function(n) {
+    // SOLVED WITH FOR LOOP
+    // let digits = Array.from(n.toString()).map(Number)
+    // let product = 1
+    // let sum = 0    
+    // for(i=0; i<digits.length; i++) {
+    //     product *= digits[i]
+    //     sum += digits[i]
+    // }
+    // return product - sum
+    
+    // SOLVED WITH REDUCERS
     let digits = Array.from(n.toString()).map(Number)
-    let product = 1
-    let sum = 0    
-    for(i=0; i<digits.length; i++) {
-        product *= digits[i]
-        sum += digits[i]
-    }
-    return product - sum      
+    let product = digits.reduce((a,b) => a*b)
+    let sum = digits.reduce((a,b) => a+b)
+    return product - sum
 };
